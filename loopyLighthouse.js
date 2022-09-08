49583c1760bf07e446dd7133a688e0b20aa453cf
Chelsea Dwarika <chelseakristina@live.com>
2022-09-08 04:44:37 +0000

    Compass Practice

---
+++ loopyLighthouse.js
@@ -0,0 +1,13 @@
+for (var x = 100; x <= 200;++x) {
+  if (x % 3 === 0){
+  console.log(“Loopy”);
+  } else if (x % 4 === 0) {
+  console.log(“Lighthouse”);
+  } else if (x % 3 === 0 && x % 4 === 0) {
+  console.log(“LoopyLighthouse”);
+  } else {
+  console.log(x);
+  }
+  }
+  
+  howManyHundreds.js loopingLastIndexOf.js loopingRange.js loopyLighthouse.js objects.js objectsAndFunctions.js objectsDrivingMayorDaisy.js objectsTrashToTreasure.js objectsWereRootingForYou.js tinkering.js votingStationCalculation.js whichSchool.js xMarksThePerfectShot.js
