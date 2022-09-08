7b480494f37de3a93a4cebde58320e97c7c3ee69
Chelsea Dwarika <chelseakristina@live.com>
2022-09-08 17:48:48 +0000

    Function practice

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,13 +1,11 @@
-for (var x = 100; x <= 200;++x) {
-  if (x % 3 === 0){
-  console.log(“Loopy”);
-  } else if (x % 4 === 0) {
-  console.log(“Lighthouse”);
-  } else if (x % 3 === 0 && x % 4 === 0) {
-  console.log(“LoopyLighthouse”);
-  } else {
-  console.log(x);
-  }
-  }
-  
-  howManyHundreds.js loopingLastIndexOf.js loopingRange.js loopyLighthouse.js objects.js objectsAndFunctions.js objectsDrivingMayorDaisy.js objectsTrashToTreasure.js objectsWereRootingForYou.js tinkering.js votingStationCalculation.js whichSchool.js xMarksThePerfectShot.js
+for (i = 100; i <= 200; i++) {
+    if (i % 3 === 0 && i % 4 === 0) {
+        console.log("LoopyLighthouse");
+    }else if (i % 3 === 0) {
+        console.log("Loopy");
+    }else if (i % 4 === 0) {
+        console.log("Lighthouse");
+    }else {
+        console.log(i);
+    }
+}
