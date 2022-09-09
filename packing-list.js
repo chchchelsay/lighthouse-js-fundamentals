0f83daa4e8e8bf7506166c152688a89f0afc88f7
Chelsea Dwarika <chelseakristina@live.com>
2022-09-09 02:42:58 +0000

    More Prep Work!

---
+++ packing-list.js
@@ -0,0 +1,18 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+/*FOR LOOP
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}*/
+
+
+//WHILE loop
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
